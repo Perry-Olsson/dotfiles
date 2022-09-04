02f63d5b229f33e78ad14943aa7463fcdd88220d
Perry <perry.olsson@smartsheet.com>
2022-09-03 17:37:05 -0700

    config

--- neovim/copy_new_config.sh
+++ neovim/copy_new_config.sh
@@ -7,6 +7,6 @@ cp $script_dir/.vimrc $HOME/.vimrc
 echo "copied new vimrc file into HOME directory"
 cp -r $script_dir/nvim/init.vim $HOME/.config/nvim
 echo "copied new init.vim file"
-cp -a $script_dir/nvim $HOME/.config/nvim
+cp -r $script_dir/nvim/lua $HOME/.config/nvim
 echo "copied nvim configuration directory"
 
