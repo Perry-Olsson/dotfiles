b90fd95a76d6bfe11fa673db5a824392174b8e3d
Perry Olsson <olsson.perry@gmail.com>
2023-06-06 23:42:37 -0700

    pull config scripts

---
+++ arch_pull_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+set -e
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+/bin/bash $script_dir/cross_platform/pull.sh
---
+++ cross_platform/pull_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+set -e
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+/bin/bash $script_dir/lunar-vim/pull_config.sh
+/bin/bash $script_dir/vscode/pull_config.sh
