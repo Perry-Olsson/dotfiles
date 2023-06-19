8328780b18684330a44070a37c23453f632e199d
Perry Olsson <olsson.perry@gmail.com>
2023-06-19 09:59:40 -0700

    refactoring nvim config

--- cross_platform/neovim/nvim/lua/config.lua
+++ cross_platform/neovim/nvim/lua/config.lua
@@ -5,7 +5,9 @@ require("vim_settings")
 require("plugins.dap")
 require("plugins.dapui")
 require("plugins.lsp")
-local opts = { noremap=true, silent=true }
+require("plugins.cmp")
+require("plugins.treesitter")
+require("plugins.treesitter")
 vim.cmd("colorscheme tokyonight")
 --[[ 
 **************************************************************
@@ -19,76 +21,6 @@ require("nvim-tree").setup({
 })
 --[[ 
 **************************************************************
-********************NVIM CMP CONFIGURATION********************
-**************************************************************
---]]
-local cmp = require'cmp'
-cmp.setup({
-snippet = {
-  expand = function(args)
-    require('luasnip').lsp_expand(args.body)
-  end,
-},
-window = {
-  completion = cmp.config.window.bordered(),
-  documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
-  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<C-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-}),
-sources = cmp.config.sources({
-  { name = 'nvim_lsp' },
-  { name = 'luasnip' }, -- For luasnip users.
-}, {
-  { name = 'buffer' },
-})
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-sources = cmp.config.sources({
-  { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-}, {
-  { name = 'buffer' },
-})
-})
-
-
---[[ 
-**************************************************************
-**********************TREESITTER CONFIG***********************
-**************************************************************
---]]
-
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "java", "kotlin" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-    indent = { enable = true },
-  },
-}
-
---[[ 
-**************************************************************
 **********************TELESCOPE CONFIG************************
 **************************************************************
 --]]
@@ -97,52 +29,3 @@ require("telescope").setup({
         file_ignore_patterns = { "%.git/", "%build/", "%.class" }
     }
 })
---[[ 
-**************************************************************
-**********************LUALINE CONFIG**************************
-**************************************************************
---]]
--- local custom_gruvbox = require'lualine.themes.gruvbox'
-
--- Change the background of lualine_c section for normal mode
--- custom_gruvbox.normal.c.bg = '#000000'
-require("lualine").setup{
-    options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
