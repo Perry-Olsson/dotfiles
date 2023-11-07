2a0b14ed11680a198c36f79c13fb378b7296968b
Perry <perry.olsson@smartsheet.com>
2023-11-07 10:40:46 -0800

    Moving from packer to lazy

--- cross_platform/neovim/nvim/lua/plugins.lua
+++ cross_platform/neovim/nvim/lua/plugins.lua
@@ -2,52 +2,53 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'mfussenegger/nvim-jdtls'
-  use 'williamboman/mason.nvim'
-  use {
-      'folke/tokyonight.nvim',
-      branch = 'main'
-  }
-  use 'EdenEast/nightfox.nvim'
-  use 'b3nj5m1n/kommentary'
-  use 'mfussenegger/nvim-dap'
-  use 'rcarriga/nvim-dap-ui'
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      ['do'] = ':TSUpdate'
-  }
-  use 'kyazdani42/nvim-web-devicons'
-  use 'kyazdani42/nvim-tree.lua'
-  use 'nvim-lua/plenary.nvim'
-  use {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.0'
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
-  use 'nvim-lualine/lualine.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'wbthomason/packer.nvim'
-  -- Lua
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-  end
-}
-end)
+vim.g.mapleader = " "
+require("lazy").setup({
+    "mfussenegger/nvim-jdtls",
+    "williamboman/mason.nvim",
+    { "folke/tokyonight.nvim", branch = "main" },
+    "EdenEast/nightfox.nvim",
+    "b3nj5m1n/kommentary",
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    { "nvim-treesitter/nvim-treesitter", ['do'] = ':TSUpdate' },
+    "kyazdani42/nvim-web-devicons",
+    "kyazdani42/nvim-tree.lua",
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope.nvim", tag = '0.1.0' },
+    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "nvim-lualine/lualine.nvim",
+    "kyazdani42/nvim-web-devicons",
+    "wbthomason/packer.nvim",
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            }
+        end
+    }
+})
