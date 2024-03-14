7af8c14cd206fd8df85755637bbcde8c5633683d
Perry Olsson <olsson.perry@gmail.com>
2024-03-13 23:07:30 -0700

    setup env file

--- wip/setup_env.sh
+++ wip/setup_env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # set -e
-mkdir $HOME/.bin
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt-get install git gh neovim
+sudo apt-get install git gh libc6-dev
+
+mkdir $HOME/.bin
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar -xzvf nvim-linux54.tar.gz -C $HOME/.bin
+echo 'export PATH="$HOME/.bin/nvim-linux-64/bin:$PATH"' >> .$HOME/.bashrc
+
 if [ "$SKIP_SSH_KEY_GEN" != "true" ]; then
   ssh-keygen -t ed25519 -C "olsson.perry@gmail.com"
   eval "$(ssh-agent -s)"
@@ -14,3 +18,6 @@ if [ "$SKIP_GH_AUTH" != "true" ]; then
   gh auth login
 fi
 git clone git@github.com:Perry-Olsson/environment-setups.git
+rm -rf $HOME/.config/nvim
+mkdir $HOME/.config/nvim
+/bin/bash SCRIPT_DIR/environment-setups/cross_platform/neovim/nvim/update_config.sh
