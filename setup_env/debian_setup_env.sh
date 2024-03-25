b6d47f99b7df1965a50dccc7a796c4776eee2418
Perry Olsson <olsson.perry@gmail.com>
2024-03-24 23:51:04 -0700

    seperate debian based command

---
+++ setup_env/debian_setup_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+update_and_install_packages () {
+    sudo apt update
+    sudo apt install git gh libc6-dev zsh neofetch dmenu xserver-xorg xinit libpangocairo-1.0-0 python3-pip python3-xcffib python3-cairocffi picom
+}
+
+update_and_install_packages()
+
+/bin/bash $SCRIPT_DIR/setup_env.sh
--- setup_env/setup_env.sh
+++ setup_env/setup_env.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-update_and_install_packages () {
-    sudo apt update
-    sudo apt install git gh libc6-dev zsh neofetch dmenu xserver-xorg xinit libpangocairo-1.0-0 python3-pip python3-xcffib python3-cairocffi picom
-}
-
 create_dirs_and_modify_path () {
     mkdir $HOME/.bin
     mkdir $HOME/.local/bin
@@ -71,8 +66,6 @@ copy_environment_config () {
     cp $SCRIPT_DIR/environment-setups/cross_platform/.zshrc $HOME/.zshrc
 }
 
-
-# update_and_install_packages
 # create_dirs_and_modify_path
 # install_neovim
 # install_fonts
