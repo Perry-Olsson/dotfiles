ff34ad124169e7a44412a5e9401b3db5b329667b
Perry Olsson <olsson.perry@gmail.com>
2024-03-24 23:55:17 -0700

    clean up

--- cross_platform/pull_config.sh
+++ cross_platform/pull_config.sh
@@ -4,7 +4,6 @@ set -e
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-/bin/bash $script_dir/lunar-vim/pull_config.sh
 /bin/bash $script_dir/vscode/pull_config.sh
 /bin/bash $script_dir/neovim/pull_config.sh
 
--- cross_platform/update_config.sh
+++ cross_platform/update_config.sh
@@ -4,7 +4,6 @@ set -e
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-/bin/bash $script_dir/lunar-vim/update_config.sh
 /bin/bash $script_dir/vscode/update_config.sh
 /bin/bash $script_dir/neovim/update_config.sh
 
