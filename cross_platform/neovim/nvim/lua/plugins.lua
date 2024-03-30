d8826a77bc6c41b9fb00d92a8a8e4dc6d58a1f91
Perry Olsson <olsson.perry@gmail.com>
2024-03-29 21:06:30 -0700

    Adding lazy git

--- cross_platform/neovim/nvim/lua/plugins.lua
+++ cross_platform/neovim/nvim/lua/plugins.lua
@@ -60,5 +60,24 @@ require("lazy").setup({
             -- refer to the configuration section below
             }
         end
+    },
+    {
+        "kdheepak/lazygit.nvim",
+    	cmd = {
+    		"LazyGit",
+    		"LazyGitConfig",
+    		"LazyGitCurrentFile",
+    		"LazyGitFilter",
+    		"LazyGitFilterCurrentFile",
+    	},
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+           { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
     }
 })
--- setup_env/debian_setup_env.sh
+++ setup_env/debian_setup_env.sh
@@ -6,6 +6,15 @@ update_and_install_packages () {
     sudo apt install git gh libc6-dev zsh neofetch dmenu xserver-xorg xinit libpangocairo-1.0-0 python3-pip python3-xcffib python3-cairocffi picom nodejs npm gcc
 }
 
-update_and_install_packages
+install_lazy_git () {
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm -rf ./lazygit.tar.gz lazygit
+}
 
-/bin/bash $SCRIPT_DIR/setup_env.sh
+#update_and_install_packages
+install_lazy_git
+
+#/bin/bash $SCRIPT_DIR/setup_env.sh
