cfaf04962ee22fbfd591e8ffa6382ed7441b9b74
Perry Olsson <olsson.perry@gmail.com>
2025-09-05 11:53:03 -0700

    starship config

---
+++ util/copy_config/starship.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+arg=$1
+
+from_dir=""
+to_dir=""
+
+# If arg is pull, copy config from system in to dotfiles repo
+# if not copy from dotfiles to system
+if [[ $arg == "pull" ]]; then
+    from_dir=$HOME/.config/starship.toml
+    to_dir=$script_dir/../../config
+else
+    from_dir=$script_dir/../../config/starship.toml
+    to_dir=$HOME/.config
+fi
+
+cp -r $from_dir $to_dir
+echo "Copied starship configuration files"
