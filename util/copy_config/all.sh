4cbec21cb4cf7e2c8e817e0c95036f093a434895
Perry Olsson <olsson.perry@gmail.com>
2025-09-11 23:19:05 -0700

    copy script fix

--- util/copy_config/all.sh
+++ util/copy_config/all.sh
@@ -4,9 +4,11 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source $script_dir/../helpers.sh
 
+arg=$1
+
 for each in $(get_config_options);
 do
     if [[ $each != "all" ]]; then
-        $script_dir/copy_config/$each.sh
+        $script_dir/copy_config/$each.sh $arg
     fi
 done
