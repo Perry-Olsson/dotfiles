0d7559ec659a68ff54c6ed2d24237ad2dc26b0b4
Perry Olsson <olsson.perry@gmail.com>
2025-09-14 10:31:00 -0700

    nvim

--- util/copy_config/nvim.sh
+++ util/copy_config/nvim.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-arg=$1
-
-from_dir=""
-to_dir=""
-
-# If arg is pull, copy config from system in to dotfiles repo
-# if not copy from dotfiles to system
-if [[ $arg == "pull" ]]; then
-    from_dir=$HOME/.config/nvim
-    to_dir=$script_dir/../../config/nvim
-else
-    from_dir=$script_dir/../../config/nvim
-    to_dir=$HOME/.config/nvim
-fi
-
-cp $from_dir/init.vim $to_dir/
-cp -r $from_dir/lua $to_dir/
-echo "Copied neovim configuration files"
+script_dir="$(dirname "$(readlink -f "$0")")"
+$script_dir/copy_dir.sh $1 nvim
