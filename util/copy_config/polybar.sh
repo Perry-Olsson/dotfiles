7878d09ad3326937fda79330a6e3d8204935664e
Perry Olsson <olsson.perry@gmail.com>
2025-09-14 10:29:38 -0700

    wip still

--- util/copy_config/polybar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Implement copy polybar files"
--- util/copy_config/zsh.sh
+++ util/copy_config/zsh.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-arg=$1
-
-from_dir=""
-to_dir=""
-
-# If arg is pull, copy config from system in to dotfiles repo
-# if not copy from dotfiles to system
-if [[ $arg == "pull" ]]; then
-    from_dir=$HOME
-    to_dir=$script_dir/../../config
-else
-    from_dir=$script_dir/../../config
-    to_dir=$HOME/
-fi
-
-cp $from_dir/.zshrc $to_dir/
-cp $from_dir/.p10k.zsh $to_dir/
-echo "Copied zsh configuration files"
+script_dir="$(dirname "$(readlink -f "$0")")"
+$script_dir/copy_file.sh $1 .zshrc
